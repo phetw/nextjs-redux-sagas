69c09ba3cdcf6b81fcbc7a7ab440d64afd80c57c
was.li <wasuwat.se@gmail.com>
2018-10-21 11:05:32 +0700

    [🎉] init redux store setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,42 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 
+import { Provider } from 'react-redux'
+import withRedux from 'next-redux-wrapper'
+import withReduxSaga from 'next-redux-saga'
+
+import createStore from '../store'
+
 import Layout from '../components/Layout/Layout'
 
-export default class MyApp extends App {
+class MyNextApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps({ ctx })
+    }
+
+    return { pageProps }
+  }
+
   componentDidCatch(error, errorInfo) {
     super.componentDidCatch(error, errorInfo)
     /* Do something */
   }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps, store } = this.props
     return (
       <Container>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
       </Container>
     )
   }
 }
+
+export default withRedux(createStore)(withReduxSaga({ async: true })(MyNextApp))
