a038b7220a94c3651de6c85c6498681a862e9bbd
was.li <wasuwat.se@gmail.com>
2018-10-21 10:17:44 +0700

    [🔧] add gzip capabilities

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const dotenv = require('dotenv')
+const express = require('express')
+const next = require('next')
+const compression = require('compression')
+
+dotenv.config()
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const port = process.env.NODE_PORT || 3000
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+    server.use(compression())
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, err => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
+  .catch(ex => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
