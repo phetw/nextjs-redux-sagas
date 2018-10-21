1a5d9661bc06333564a037b6c1fedea903330779
was.li <wasuwat.se@gmail.com>
2018-10-21 19:20:27 +0700

    [🔧] update api

--- services/api/index.js
+++ services/api/index.js
@@ -4,6 +4,6 @@ const GITHUB_SEARCH = ' https://api.github.com/search'
 
 export default class API {
   static get() {
-    return axios.get(GITHUB_SEARCH + '/users?q=type:user+location:thailand+repos:%3E30')
+    return axios.get(GITHUB_SEARCH + '/users?q=type:user+location:thailand+repos:%3E50&sort=followers&page=1&per_page=30')
   }
 }
